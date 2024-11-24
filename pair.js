787176164344dc634900d6dcb2b67f15df3c8c33
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-11-24 18:01:36 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,14 +57,14 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: '' + b64data });
 
                let SIGMA_MD_TEXT = `
-*SUPER MD ONLINE*
+*CRAZY BOT ONLINE*
 ____________________________________
 ╔════◇
 ║『 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍𝙎』
 
-║ ❒ *Trevley* : _https://wa.me/254729980068_
+║ ❒ *CRAZY* : _https://wa.me/254729980068_
 
-║ ❒ *Ibrahim Adams* : _https://wa.me/message/74F2PC4JA4F3P1_
+║ ❒ *DULLAH MD* : _https://wa.me/message/74F2PC4JA4F3P1_
 
 ╚════════════════════❒
 
